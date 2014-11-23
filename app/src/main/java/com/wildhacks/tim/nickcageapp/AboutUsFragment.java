8a4b77f89dc5b1f36b7e2863f4b6e86aaded0f29
timchoh585 <timchoh@gmail.com>
2014-11-23 05:01:00 -0600

    fixed another text problem

--- app/src/main/java/com/wildhacks/tim/nickcageapp/AboutUsFragment.java
+++ app/src/main/java/com/wildhacks/tim/nickcageapp/AboutUsFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * Created by tim on 11/22/14.
@@ -16,9 +15,6 @@ public class AboutUsFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup view,
                              Bundle savedInstanceState)
     {
-        TextView text = (TextView) getView().findViewById(R.id.nickBio);
-        text.setText("I are Nick Cage.\nI good actor.");
-
         View rootView = inflater.inflate(R.layout.about_frag, view, false);
 
         return rootView;
